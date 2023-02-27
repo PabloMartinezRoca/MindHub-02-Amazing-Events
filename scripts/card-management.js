0b36b3d42940c44474bf2cf533d3a002b5c71f91
Pablo Martínez Roca <pablo.martinezroca@d-mice.com>
2023-02-27 14:04:17 -0300

    css implementation + javascript

--- scripts/card-management.js
+++ scripts/card-management.js
@@ -1,78 +1,136 @@
-function getEvents(data, timeEvents = null) { // 'upcoming' , 'past'
-	let eventsList = []
+function getEvents(data, timeEvents = null) {
+  // 'upcoming' , 'past'
+  let eventsList = [];
 
-	for (let eventDetails of data.events.filter((event) =>
-		timeEvents == "upcoming"
-			? event.date > data.currentDate
-			: timeEvents == "past"
-				? event.date < data.currentDate
-				: event.date == event.date
-	)) {
-		eventsList.push(eventDetails)
-	}
+  for (let eventDetails of data.events.filter((event) =>
+    timeEvents == "upcoming"
+      ? event.date > data.currentDate
+      : timeEvents == "past"
+      ? event.date < data.currentDate
+      : event.date == event.date
+  )) {
+    eventsList.push(eventDetails);
+  }
 
-	sortEvents(eventsList, "desc")
+  sortEvents(eventsList, "desc");
 
-	return eventsList
+  return eventsList;
 }
 
 function sortEvents(eventsList, order = "asc") {
-	if (order == "asc") {
-		eventsList.sort((a, b) => a.date > b.date ? 1 : 0)
-	}
-	else {
-		eventsList.sort((a, b) => (a.date < b.date ? 1 : 0))
-	}
-	return eventsList
+  if (order == "asc") {
+    eventsList.sort((a, b) => (a.date > b.date ? 1 : a.date < b.date ? -1 : 0));
+  } else {
+    eventsList.sort((a, b) => (a.date < b.date ? 1 : a.date > b.date ? -1 : 0));
+  }
+}
+
+function getCategoryList(eventsList) {
+  let categoryEventsList = [];
+
+  for (let category of eventsList) {
+    if (!categoryEventsList.includes(category.category)) {
+      categoryEventsList.push(category.category);
+    }
+  }
+
+  sortCategories(categoryEventsList, "asc");
+
+  return categoryEventsList;
+}
+
+function sortCategories(categoryEventsList, order = "asc") {
+  if (order == "asc") {
+    categoryEventsList.sort((a, b) => (a > b ? 1 : a < b ? -1 : 0));
+  } else {
+    categoryEventsList.sort((a, b) => (a < b ? 1 : a > b ? -1 : 0));
+  }
+  return categoryEventsList;
 }
 
 function getCard(eventDetails) {
-
+  
 	// Set Premiere Events
-	let premiere = eventDetails.date > data.currentDate ? "¡COMING SOON!" : "&nbsp;"
-	let premiereDate = eventDetails.date > data.currentDate ? "PREMIERE ON " + new Date(eventDetails.date).toLocaleDateString("en-US") : "SINCE " + new Date(eventDetails.date).toLocaleDateString("en-US")
+	let premiere = ""
+  let premiereDate = ""
+	
+	if(eventDetails.date > data.currentDate)
+	{
+		premiere = "¡COMING SOON!"
+		
+		let days = new Date(eventDetails.date).getTime() - new Date(data.currentDate).getTime() 
+		days = days / (1000 * 60 * 60 * 24)
+		
+		premiereDate = "PREMIERE ON " + new Date(eventDetails.date).toLocaleDateString("en-US") + " • " + days + (days > 1 ? " DAYS" : " DAY") + " LEFT"
+  }
+	else
+	{
+		premiereDate = "SINCE " + new Date(eventDetails.date).toLocaleDateString("en-US")
+	}
 
-	// Card Templates
-	cardTemplate = `
+  // Card Templates
+  cardTemplate = `
 		<!-- Card -->
-		<div class="event-card col h-100">
-			<div class="card shadow-sm">
+		<div class="event-card card shadow">
 				
-				<div class="card-header text-center">${premiere}</div>
+			<div class="card-header text-center">${premiere}</div>
 
-				<div class="img-card-container d-flex">
-					<img class="object-fit-cover" src="${eventDetails.image}" alt="">
-				</div>
+			<div class="card-image-container">
+				<img class="object-fit-cover" src="${eventDetails.image}" alt="">
+			</div>
 
-				<h5 class="card-title bg-white bg-opacity-75 text-center">${eventDetails.name}</h5>
-				<div class="card-body">
-					<h6 class="card-subtitle small mb-4"><span class="text-muted">Category : </span><span class="strong">${eventDetails.category}</span></h6>
-					<p class="card-text">${eventDetails.description}</p>
+			<h5 class="event-title bg-white bg-opacity-90 mb-0 text-center">${eventDetails.name}</h5>
+
+			<h6 class="event-category small mb-2"><span class="text-muted">Category : </span><span class="strong">${eventDetails.category}</span></h6>
+
+			<div class="card-body d-flex flex-column justify-content-between">					
+				<p class="event-description mb-4">${eventDetails.description}</p>
 				
-						<div class="d-flex justify-content-between align-items-center">
+				<div>
+					<div class="d-flex justify-content-between align-items-center">
 						<div class="btn-group">
-							<button type="button" class="btn btn-sm btn-outline-secondary" onclick="location.href='./details.html?id=${eventDetails._id}'">View event >></button>
+							<a href="#" onclick="location.href='./details.html?id=${eventDetails._id}'" class="btn btn-sm btn-outline-amazing"><span class="pe-2">View event</span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#cc0066"  viewBox="0 0 16 16"><path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" /></svg></a>							
 						</div>
-						<small class="text-muted">$ ${eventDetails.price}</small>
+					
+						<small class="event-price">Price $ ${eventDetails.price}</small>
 					</div>
 				</div>
-				
-				<div class="card-footer">
-					<small class="text-muted">${premiereDate}</small>
-				</div>
+			</div>
+			
+			<div class="card-footer">
+				<p class="event-premiere">${premiereDate}</p>
 			</div>
 		</div>
 		<!-- end card -->
-		`
+		`;
 
-	return cardTemplate
+  return cardTemplate;
 }
 
 function insertCardsInDOM(containerID, data, timeEvents = null) {
-	let container = document.getElementById(containerID)
+  let container = document.getElementById(containerID);
 
-	for (let eventDetails of getEvents(data, timeEvents)) {
-		container.innerHTML += getCard(eventDetails)
-	}
+  let eventsList = getEvents(data, timeEvents);
+  let categoryEventsList = getCategoryList(eventsList);
 
+  for (let eventDetails of eventsList) {
+    container.innerHTML += getCard(eventDetails);
+  }
+
+  insertFilterBar(categoryEventsList);
+}
+
+function insertFilterBar(categoryEventsList) {
+  let filterBarContainer = document.getElementById("filter-bar-container");
+
+  for (let category of categoryEventsList) {
+    let filterBarTemplate = `
+		<label class="filter-bar-item">
+			<input class="form-check-input flex-shrink-0 me-2" type="checkbox" value="${category}">
+			<span>${category}</span>
+		</label>
+		`;
+
+    filterBarContainer.innerHTML += filterBarTemplate;
+  }
 }
